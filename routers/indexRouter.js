dc3ee7926d0e7727fd70c9fb087709e815e974c9
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-08 21:14:11 +0500

    Render the form view and handle it in POST method

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,11 +3,13 @@ const indexRouter = Router();
 
 const messages = [
   {
+    id: 0,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 1,
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -16,6 +18,26 @@ const messages = [
 
 indexRouter.get("/", (req, res) => {
   res.render("index", { messages: messages });
+});
+
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+  const reqBody = req.body;
+
+  const messageText = req.body.message;
+  const messageAuthor = req.body.author;
+
+  messages.push({
+    id: messages.length,
+    text: messageText,
+    user: messageAuthor,
+    added: new Date(),
+  });
+
+  res.redirect("/");
 });
 
 module.exports = indexRouter;
