73450012ad1f68b110a2fbd19d47c81b5b56afdd
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-08 21:14:53 +0500

    Add middleware to parse the data from req.body

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 
+const indexRouter = require('./routers/indexRouter');
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/', indexRouter);
 
 const PORT = 3000;
 
