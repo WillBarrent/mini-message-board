77c3a08a668a1649b25938d0571dc106b8b6f732
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-15 16:40:43 +0500

    Use new route

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const app = express();
 
 require("dotenv").config();
 
-const indexRouter = require('./routers/indexRouter');
+const indexRouter = require("./routers/indexRouter");
+const newUserRouter = require("./routers/newUserRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+app.use("/new", newUserRouter);
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log("App is listening on PORT", PORT);
+  console.log("App is listening on PORT", PORT);
 });
