947349503802577805302a0bed3b6353551ee8d0
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-15 15:47:25 +0500

    Change connection config

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,5 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.HOST, 
-  user: process.env.DATABASE,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT
+  connectionString: process.env.CONNECTION,
 });
