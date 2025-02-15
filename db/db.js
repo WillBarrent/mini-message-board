ea02e81359f0d47f50652515f970fe78355686d6
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-15 15:47:48 +0500

    Create all necessary methods to work with db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,24 @@
 const pool = require("./pool");
 
-async function getAllMessages() {}
+async function getAllMessages() {
+  const { rows } = pool.query("SELECT * FROM messages");
+  return rows;
+}
 
-async function getMessageById(id) {}
+async function getMessageById(id) {
+  const { rows } = pool.query("SELECT * FROM messages WHERE id = ($1)", [id]);
+  return rows;
+}
 
-async function createMessage() {}
+async function createMessage(username, message, added) {
+  await pool.query(
+    "INSERT INTO usernames (username, message, added) VALUES ($1, $2, $3)",
+    [username, message, added]
+  );
+}
+
+module.exports = {
+  getAllMessages,
+  getMessageById,
+  createMessage,
+};
